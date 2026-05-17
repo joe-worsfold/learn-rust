1c5ef6a12dfeb3b65c92c8b3ef461cae1c785fc9
joe-worsfold <22414014+joe-worsfold@users.noreply.github.com>
2026-05-17 17:21:05 +0200

    feat: loops

---
+++ branches/src/main.rs
@@ -0,0 +1,49 @@
+fn main() {
+
+    for number in (1..4).rev() {
+         println!("{number}");
+    }
+    
+    let a = [10, 20, 30, 40, 50];
+    for element in a {
+         println!("the value is: {element}");
+    }
+
+
+    let mut count: i8 = 0;
+    'counting_up: loop {
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop {
+            println!("remaining = {remaining}");
+            if remaining == 9 {
+                break;
+            }
+            if count == 2 {
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+
+    let number = 6;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    } else {
+        println!("number is not divisible by 4, 3, or 2");
+    }
+
+    let condition = true;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
+}
