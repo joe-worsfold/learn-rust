db731e7d744b68d84c45442517f24d20c56dddbb
joe-worsfold <22414014+joe-worsfold@users.noreply.github.com>
2026-06-07 15:01:14 +0100

    structs

---
+++ structs/src/main.rs
@@ -0,0 +1,31 @@
+struct Color(i32, i32, i32); // named tuples
+struct Point(i32, i32, i32);
+struct AlwaysEqual; // trait definitions tbc
+
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
+}
+
+fn build_user(email: String, username: String) -> User {
+    User {
+        active: true,
+        username,
+        email,
+        sign_in_count: 1,
+    }
+}
+
+fn main() {
+    let user1 = build_user(
+        String::from("someone@example.com"),
+        String::from("someusername123"),
+    );
+
+    let user2 = User {
+        email: String::from("another@example.com"),
+        ..user1
+    };
+}
